c60a436e79b9139ffb5260e454d7f6e419ce2667
zangue-mbcode <zanulrich@gmail.com>
2023-11-21 17:35:09 +0100

    add account

--- pages/api/[...nextauth].ts
+++
@@ -1,8 +0,0 @@
-
-import { authOptions } from '@/lib/auth/nextauth-option';
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth from 'next-auth';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, authOptions(req, res));
-}
