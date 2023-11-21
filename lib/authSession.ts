fd8da573e404efc97edd5bee4d3c4f2f61b3b4ff
zangue-mbcode <zanulrich@gmail.com>
2023-11-21 17:39:36 +0100

    add account

--- lib/authSession.ts
+++ lib/authSession.ts
@@ -1,16 +1,14 @@
-import { useRouter } from "next/navigation";
+
 import { supabase } from "./supabase";
 
 
 export const getAuthSession = async () => {
-    const router = useRouter()
     
         const { data, error } = await supabase.auth.getSession()
         if (error) {
          return data;
         } else {
          if (!data.session) {
-          router.push("/")
           return data;
          }
     
