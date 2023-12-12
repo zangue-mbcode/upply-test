6214c1138d0c20a6ebff92c97e82e6af79758eda
zangue-mbcode <zanulrich@gmail.com>
2023-12-12 18:44:07 +0100

    home page

---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import HomeView from "@/components/views/HomeView/HomeView"
+
+
+
+export default function AuthenticationPage() {
+  return (
+    <HomeView />
+  )
+}
---
+++ components/views/HomeView/HomeView.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+
+
+const HomeView = () => {
+  
+  return (
+    <>
+        <div className='w-full h-10 mb-5 border border-1 border-inherit border-dotted rounded'>
+gffgsddfg
+        </div>
+    </>
+  );
+};
+
+export default HomeView;
