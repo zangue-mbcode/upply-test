c16f7e0714a7052211da698a3344a3be8b31348f
zangue-mbcode <zanulrich@gmail.com>
2023-12-19 14:09:51 +0100

    test

--- components/features/assemblies/homeAssemblies/header-centered-items.tsx
+++ components/features/assemblies/homeAssemblies/header-centered-items.tsx
@@ -74,7 +74,7 @@ export const HeaderCenteredItems = async () => {
                                     clip-rule="evenodd" 
                                     d="M0.5 5C0.5 4.44772 0.947715 4 1.5 4L18.5 4C19.0523 4 19.5 4.44772 19.5 5C19.5 5.55229 19.0523 6 18.5 6L1.5 6C0.947715 6 0.5 5.55228 0.5 5Z"
                                     fill="current"
-                                    className="pathOne"
+                                    className="pathOne text-white dark:text-black"
                                 >
                                 </path>
                                 <path 
@@ -82,7 +82,7 @@ export const HeaderCenteredItems = async () => {
                                     clip-rule="evenodd" 
                                     d="M15.2071 0.292893C14.8166 -0.0976311 14.1834 -0.0976311 13.7929 0.292893C13.4024 0.683418 13.4024 1.31658 13.7929 1.70711L17.0858 5L13.793 8.29274C13.4025 8.68327 13.4025 9.31643 13.793 9.70696C14.1836 10.0975 14.8167 10.0975 15.2073 9.70696L19.2071 5.70711L19.2073 5.70696C19.5978 5.31643 19.5978 4.68327 19.2073 4.29274C19.2023 4.28781 19.1974 4.28294 19.1924 4.27814L15.2071 0.292893Z"
                                     fill="current"
-                                    className="pathTwo"
+                                    className="pathTwo text-white dark:text-black"
                                 >
                                 </path>
                             </svg>
@@ -98,7 +98,7 @@ export const HeaderCenteredItems = async () => {
                                     clip-rule="evenodd" 
                                     d="M0.5 5C0.5 4.44772 0.947715 4 1.5 4L18.5 4C19.0523 4 19.5 4.44772 19.5 5C19.5 5.55229 19.0523 6 18.5 6L1.5 6C0.947715 6 0.5 5.55228 0.5 5Z"
                                     fill="current"
-                                    className="pathOne"
+                                    className="pathOne text-black dark:text-white"
                                 >
                                 
                                 </path>
@@ -107,7 +107,7 @@ export const HeaderCenteredItems = async () => {
                                     clip-rule="evenodd" 
                                     d="M15.2071 0.292893C14.8166 -0.0976311 14.1834 -0.0976311 13.7929 0.292893C13.4024 0.683418 13.4024 1.31658 13.7929 1.70711L17.0858 5L13.793 8.29274C13.4025 8.68327 13.4025 9.31643 13.793 9.70696C14.1836 10.0975 14.8167 10.0975 15.2073 9.70696L19.2071 5.70711L19.2073 5.70696C19.5978 5.31643 19.5978 4.68327 19.2073 4.29274C19.2023 4.28781 19.1974 4.28294 19.1924 4.27814L15.2071 0.292893Z"
                                     fill="current"
-                                    className="pathTwo"
+                                    className="pathTwo text-black dark:text-white"
                                 >
                                 </path>
                             </svg>
